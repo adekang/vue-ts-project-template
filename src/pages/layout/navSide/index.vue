dde18ad08f37e6d056888153dad1d0747c58415b
adekang <adekang@163.com>
2024-04-30 22:16:36 +0800

    feat: 布局

---
+++ src/pages/layout/navSide/index.vue
@@ -0,0 +1,27 @@
+<script lang='ts' setup>
+import MMenu from '@/components/menu/index.vue'
+
+defineProps<{
+  collapse: boolean
+}>()
+
+const data = [
+  {
+    icon: 'HomeFilled',
+    name: '首页',
+    index: '/home',
+  },
+  {
+    icon: 'ShoppingBag',
+    name: 'Pro 表格',
+    index: '/table',
+  },
+]
+</script>
+
+<template>
+  <MMenu :collapse="collapse" :data="data" router :default-active="$route.path" />
+</template>
+
+<style  scoped>
+</style>
